2fa5fe3f80932b96b5f775511fdf4ee869c6a4b6
Diego <166749415+Di3go07@users.noreply.github.com>
2024-06-15 21:57:52 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ data = pd.read_csv(r'./tabela_users.csv', sep=',')
 #FUNÇÕES
 
 #criando usuarios
-cargos = ['gerente','administrador','funcionario','cliente']
+cargo = ['gerente','administrador','funcionario','cliente']
 def criar_usuarios():
     print('-'*50)
     criar_id = int(input('Digite um id: '))
@@ -20,7 +20,7 @@ def criar_usuarios():
     confirmar_senha = str(input('Confirme a senha: '))
     if criar_senha == confirmar_senha:
         criar_cargo = str(input('Qual o cargo do usuário: '))
-        if criar_cargo in cargos:
+        if criar_cargo in cargo:
             nv_user = [criar_id, criar_user, confirmar_senha, criar_cargo]
             with open('./tabela_users.csv', 'a') as f_object: 
                 writer_object = writer(f_object) 
@@ -33,7 +33,7 @@ def criar_usuarios():
             print('Opções: gerente, administrador, funcionario e cliente')
             retomada = criar_usuarios()
     else:
-        print('Senhas não coincidem, tente novamente...')
+        print('senha não coincidem, tente novamente...')
         retomada = criar_usuarios()
 
 #página login usuários
@@ -41,7 +41,7 @@ def login_user():
     print('-'*50)
     with open('./tabela_users.csv', 'a') as f_object: 
         user_atual = input('Usuario: ')
-        senha_atual = input('Senha: ')
+        senha_atual = getpass.getpass('Senha: ') #a senha não aparece ao ser digitada
         verficacao = verificar_usuarios(user_atual, senha_atual)
         quebra = usuario_linha(user_atual)
     f_object.close() 
@@ -49,7 +49,7 @@ def login_user():
 #verificando usuarios
 def verificar_usuarios(user, senha):
     with open('./tabela_users.csv', 'a') as f_object: 
-        if user in data['nomes'].values and senha in data['senhas'].values:
+        if user in data['nomes'].values and senha in data['senha'].values:
             print('Bem vindo!')
         else:
             print('Usuário não encontrado')
@@ -62,43 +62,73 @@ def verificar_usuarios(user, senha):
 
 #página de habilidades dos usuários
 def usuario_linha(user):
+    global data
     print('-'*50)
     quebra = data['nomes'] == user
     linha_user = data[quebra]
 
     #gerente
-    if linha_user['cargos'].unique() == 'gerente':
-        print('1 - arquivos')
+    if linha_user['cargo'].unique() == 'gerente':
+        print('Acessando como gerente')
+        print('1 - inventários')
         print('2 - usuarios')
         resposta = int(input('O que você deseja manipular: '))
         if resposta == 1:
-            print('1 - Ler arquivo')
-            print('2 - Editar arquivo')
-            print('3 - Adicionar arquivo')
-            print('4 - Excluir arquivo')
-            arq_resposta = int(input('Opção: '))
+            print('1 - Ler inventário')
+            print('2 - Editar inventário')
+            print('3 - Adicionar inventário')
+            print('4 - Excluir inventário')
+            gerente_resposta = int(input('Opção: '))
+
         elif resposta == 2:
             print('1 - Tabela usuarios')
             print('2 - Adicionar usuario')
             print('3 - Excluir usuario')
-            usr_resposta = int(input('Opção: '))
+            gerente_resposta = int(input('Opção: '))
+            if gerente_resposta == 1:
+                print(data)
+                sequencia = usuario_linha(user)
+            if gerente_resposta == 2:
+                sequencia = criar_usuarios()
+            if gerente_resposta == 3:
+                sequencia = del_user()
 
     #administrador
-    elif linha_user['cargos'].unique() == 'administrador':
-            print('1 - Ler arquivo')
-            print('2 - Editar arquivo')
-            print('3 - Adicionar arquivo')
-            arq_resposta = int(input('Opção: '))
+    elif linha_user['cargo'].unique() == 'administrador':
+            print('Acessando como administrador')
+            print('1 - Ler inventário')
+            print('2 - Editar inventário')
+            print('3 - Adicionar inventário')
+            print('4 - Tabela usuarios')
+            adm_resposta = int(input('Opção: '))
 
     #funcionario
-    elif linha_user['cargos'].unique() == 'funcionario':
-            print('1 - Ler arquivo')
-            print('2 - Adicionar arquivo')
-            arq_resposta = int(input('Opção: '))
+    elif linha_user['cargo'].unique() == 'funcionario':
+            print('Acessando como funcionario')
+            print('1 - Ler inventário')
+            print('2 - Adicionar inventário')
+            func_resposta = int(input('Opção: '))
 
     #cliente
-    else:
+    elif linha_user['cargo'].unique() == 'cliente':
+        print('Acessando como cliente')
         print(data) #PRECISA MUDAR DEPOIS
+
+
+def del_user():
+    print('Deletar usuário')
+    del_user = input('Usuário: ')
+    global data
+    with open('./tabela_users.csv', 'r') as file:
+        reader = csv.reader(file)
+        linhas = list(reader)
+    linhas_filtradas = [linha for linha in linhas if linha[1] != del_user]
+    with open('./tabela_users.csv', 'w', newline='') as file: 
+        writer = csv.writer(file)
+        writer.writerows(linhas_filtradas)
+        print('Usuário excluido!')
+        print('Abra novamente o programa')
+
 
 #HEADER
 print('')
