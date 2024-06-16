e2fbe1549f98f484d55c7a74d32d57a792a0a335
Diego <166749415+Di3go07@users.noreply.github.com>
2024-06-16 11:14:58 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -5,36 +5,11 @@ import pandas as pd
 
 #BANCO DE DADOS
 data = pd.read_csv(r'./tabela_users.csv', sep=',')
+data_prod = pd.read_csv(r'./tabela_produtos.csv', sep=',')
 #abre o banco de dados
 
 
 #FUNÇÕES
-
-#criando usuarios
-cargo = ['gerente','administrador','funcionario','cliente']
-def criar_usuarios():
-    print('-'*50)
-    criar_id = int(input('Digite um id: '))
-    criar_user = str(input('Nome do usuário: '))
-    criar_senha = str(input('Senha: '))
-    confirmar_senha = str(input('Confirme a senha: '))
-    if criar_senha == confirmar_senha:
-        criar_cargo = str(input('Qual o cargo do usuário: '))
-        if criar_cargo in cargo:
-            nv_user = [criar_id, criar_user, confirmar_senha, criar_cargo]
-            with open('./tabela_users.csv', 'a') as f_object: 
-                writer_object = writer(f_object) 
-                writer_object.writerow(nv_user) 
-            f_object.close() 
-            print('Usuario cadastrado!')
-            print('Abra novamente o programa')
-        else:
-            print('Cargo não existe!')
-            print('Opções: gerente, administrador, funcionario e cliente')
-            retomada = criar_usuarios()
-    else:
-        print('senha não coincidem, tente novamente...')
-        retomada = criar_usuarios()
 
 #página login usuários
 def login_user():
@@ -61,6 +36,7 @@ def verificar_usuarios(user, senha):
     f_object.close()     
 
 #página de habilidades dos usuários
+
 def usuario_linha(user):
     global data
     print('-'*50)
@@ -75,11 +51,17 @@ def usuario_linha(user):
         resposta = int(input('O que você deseja manipular: '))
         if resposta == 1:
             print('1 - Ler inventário')
-            print('2 - Editar inventário')
-            print('3 - Adicionar inventário')
-            print('4 - Excluir inventário')
+            print('2 - Editar produto')
+            print('3 - Adicionar produto')
+            print('4 - Excluir produto')
             gerente_resposta = int(input('Opção: '))
-
+            if gerente_resposta == 1:
+                print('Tabela produtos')
+                print(data_prod) 
+            if gerente_resposta == 3:
+                sequencia = criar_produto()
+            if gerente_resposta == 4:
+                sequencia = del_prods()
         elif resposta == 2:
             print('1 - Tabela usuarios')
             print('2 - Adicionar usuario')
@@ -97,8 +79,8 @@ def usuario_linha(user):
     elif linha_user['cargo'].unique() == 'administrador':
             print('Acessando como administrador')
             print('1 - Ler inventário')
-            print('2 - Editar inventário')
-            print('3 - Adicionar inventário')
+            print('2 - Editar produto')
+            print('3 - Adicionar produto')
             print('4 - Tabela usuarios')
             adm_resposta = int(input('Opção: '))
 
@@ -106,28 +88,120 @@ def usuario_linha(user):
     elif linha_user['cargo'].unique() == 'funcionario':
             print('Acessando como funcionario')
             print('1 - Ler inventário')
-            print('2 - Adicionar inventário')
+            print('2 - Adicionar produto')
             func_resposta = int(input('Opção: '))
 
     #cliente
     elif linha_user['cargo'].unique() == 'cliente':
         print('Acessando como cliente')
-        print(data) #PRECISA MUDAR DEPOIS
+        print('Tabela produtos')
+        print(data_prod) 
 
+#habilidades dos usuários
 
+#criar usuarios
+cargo = ['gerente','administrador','funcionario','cliente']
+def criar_usuarios():
+    print('-'*50)
+    criar_id = int(input('Digite um id: '))
+    criar_user = str(input('Nome do usuário: '))
+    criar_senha = str(input('Senha: '))
+    confirmar_senha = str(input('Confirme a senha: '))
+    if criar_senha == confirmar_senha:
+        criar_cargo = str(input('Qual o cargo do usuário: '))
+        if criar_cargo in cargo:
+            nv_user = [criar_id, criar_user, confirmar_senha, criar_cargo]
+            with open('./tabela_users.csv', 'a') as f_object: 
+                writer_object = writer(f_object) 
+                writer_object.writerow(nv_user) 
+            f_object.close() 
+            print('Usuario cadastrado!')
+            print('Abra novamente o programa')
+        else:
+            print('Cargo não existe!')
+            print('Opções: gerente, administrador, funcionario e cliente')
+            retomada = criar_usuarios()
+    else:
+        print('senha não coincidem, tente novamente...')
+        retomada = criar_usuarios()
+
+#excluir usuário
 def del_user():
     print('Deletar usuário')
+    del_id = int(input('Id: '))
     del_user = input('Usuário: ')
     global data
     with open('./tabela_users.csv', 'r') as file:
         reader = csv.reader(file)
         linhas = list(reader)
-    linhas_filtradas = [linha for linha in linhas if linha[1] != del_user]
+    linhas_filtradas = [linha for linha in linhas if len(linha) > 1 and linha[0] != del_id and linha[1] != del_user]
     with open('./tabela_users.csv', 'w', newline='') as file: 
         writer = csv.writer(file)
         writer.writerows(linhas_filtradas)
         print('Usuário excluido!')
         print('Abra novamente o programa')
+
+#criar produto
+def criar_produto():
+    parar = False
+    produto_id = int(input('Digite um id: '))
+    produto_nome = str(input('Material: '))
+    produto_preco = float(input('Preço por unidade: '))
+    produto_quant = int(input('Quantidade: '))
+    with open('./tabela_produtos.csv', 'r') as file:
+        reader = csv.reader(file)
+        linhas = list(reader)
+        for linha in linhas:
+            if len(linha) > 1 and linha[1].lower() == produto_nome.lower():
+                print('Produto já foi adicionado')
+                parar = True
+    if parar != True:
+        with open('./tabela_produtos.csv', 'a') as f_object: 
+            nv_produto = [produto_id, produto_nome, produto_preco, produto_quant]
+            writer_object = writer(f_object) 
+            writer_object.writerow(nv_produto) 
+            f_object.close() 
+            print('Produto adicionado!')
+            print('Abra novamente o programa')
+    elif parar == True:
+        ver_tabela = input('Deseja consultar a tabela [Y/n]: ')
+        if ver_tabela == 'Y':
+            print('Tabela produtos')
+            print(data_prod) 
+        elif ver_tabela == 'n':
+            print('Fim')
+
+#excluir produto
+def del_prods():
+    parar = False
+    produtos = []
+    print('Deletar produto')
+    del_id = int(input('Id: '))
+    del_prod = input('Produto: ')
+    global data
+    with open('./tabela_produtos.csv', 'r') as file:
+        reader = csv.reader(file)
+        linhas = list(reader)
+        for lista in linhas:
+            if len(lista) > 1:
+                produtos.append(lista[1])
+    if del_prod not in produtos:
+        parar = True
+    if parar != True:
+        linhas_filtradas = [linha for linha in linhas if len(linha) > 1 and linha[0] != del_id and linha[1].lower() != del_prod.lower()]
+        with open('./tabela_produtos.csv', 'w', newline='') as file: 
+            writer = csv.writer(file)
+            writer.writerows(linhas_filtradas)
+            print('Produto excluido!')
+            print('Abra novamente o programa')
+    elif parar == True:
+        print('Produto não encontrado')
+        ver_tabela = input('Deseja consultar a tabela [Y/n]: ')
+        if ver_tabela == 'Y':
+            print('Tabela produtos')
+            print(data_prod) 
+        elif ver_tabela == 'n':
+            print('Fim')
 
 
 #HEADER
